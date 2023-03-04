03b2616de89593fd03774b4eab1820d7d0ca5024
Vishavjit Singh <64301375+worldwinner-vishav@users.noreply.github.com>
2023-03-04 11:37:47 +0530

    Create Jun 17.py

---
+++ June/Jun 17.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countSubarrays(self, nums, minK, maxK):
+        minfound = maxfound = False
+        start = minstart = maxstart = 0
+        total = 0
+
+        for i in range(len(nums)):
+            if nums[i] == minK:
+                minstart = i
+                minfound = True
+            if nums[i] == maxK:
+                maxstart = i
+                maxfound = True
+            if not minK <= nums[i] <= maxK:
+                minfound = maxfound = False
+                start = i+1
+            if minfound and maxfound:
+                total += min(minstart,maxstart) - start + 1
+
+        return total
