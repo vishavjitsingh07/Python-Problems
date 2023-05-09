58912a5f441a5cbfa3507d7b3d5dc6bb72262bd8
Vishavjit Singh <64301375+worldwinner-vishav@users.noreply.github.com>
2023-05-09 22:47:55 +0530

    Create Aug 09.py

---
+++ August/Aug 09.py
@@ -0,0 +1,39 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        row =  0
+        col = 0
+        n = len(matrix)
+        m = len(matrix[0])
+        endRow = n
+        endCol = m
+        startRow = -1
+        startCol = -1
+        arr = []
+        while len(arr) < n*m:
+            while col < endCol:
+                arr.append(matrix[row][col])
+                col+=1
+            startRow+=1
+            row+=1
+            col -= 1
+            while row < endRow:
+                arr.append(matrix[row][col])
+                row += 1
+            endCol -= 1
+            row-=1
+            col-=1
+
+            while col > startCol:
+                arr.append(matrix[row][col])
+                col-=1
+
+            startCol += 1
+            col+=1
+            row-=1
+            while row > startRow:
+                arr.append(matrix[row][col])
+                row -=1
+            endRow -= 1
+            row += 1
+            col+=1
+        return arr[:n*m]
