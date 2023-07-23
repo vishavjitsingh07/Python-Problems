353c50dc29a58af572cb97a507da3a26374f07da
Vishavjit Singh <64301375+worldwinner-vishav@users.noreply.github.com>
2023-07-23 22:16:38 +0530

    Create Oct 18.py

---
+++ October/Oct 18.py
@@ -0,0 +1,27 @@
+class Solution:
+    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+        if n % 2 == 0:
+            return []
+
+        memo = {}
+
+        def _allPossibleFBT(n):
+            if n in memo:
+                return memo[n]
+
+            list = []
+            if n == 1:
+                list.append(TreeNode(0))
+            else:
+                for i in range(1, n - 1, 2):
+                    lTrees = _allPossibleFBT(i)
+                    rTrees = _allPossibleFBT(n - i - 1)
+
+                    for lt in lTrees:
+                        for rt in rTrees:
+                            list.append(TreeNode(0, lt, rt))
+
+            memo[n] = list
+            return list
+
+        return _allPossibleFBT(n)
