5bfd53b4b3b17714531f5f01609eeee7368f18a9
Vishavjit Singh <64301375+worldwinner-vishav@users.noreply.github.com>
2023-09-13 22:06:07 +0530

    Create November 23.py

---
+++ November 23.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def candy(self, ratings):
+        n = len(ratings)
+        arr = [1]*n
+        for i in range(1, n):
+            if ratings[i]> ratings[i-1]:
+                arr[i] = max(1+ arr[i-1], arr[i])
+        for i in range(n-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                arr[i] = max(1 + arr[i+1], arr[i])
+        return sum(arr)
+     
+        
